4b43217ab73aa047cc2bff1dea15a75cecaa7440
Aelixjp <b.rayan991@hotmail.com>
2020-02-28 23:43:37 -0500

    Mobile Calculator

---
+++ README.md
@@ -0,0 +1,5 @@
+# MobileCalculator
+Calculadora simple para moviles hecha en android studio.
+
+
+Aviso: usar la carpeta Calculator en vez de MobileCalculator para importar el proyecto.
---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
